dddbb2f9ce68033d4c5c75116efdd6d05599c658
kayongo <kayongokayhood@gmail.com>
2025-05-10 13:29:17 +0200

    removed struct folder

--- struct/response.go
+++
@@ -1,7 +0,0 @@
-package struct 
-type Response struct {
-	Status  int         `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-	Meta    interface{} `json:"meta,omitempty"`
-}
